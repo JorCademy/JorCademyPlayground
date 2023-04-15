5b32c0771118829521004934b1b28e7e1c238946
Nick Jordan <nick@jorcademy.nl>
2023-04-15 15:55:05 +0200

    Minor changes

--- Playground.cs
+++ Playground.cs
@@ -1,19 +1,14 @@
-using System;
-using System.Net.Mime;
 using Microsoft.Xna.Framework;
 
 public partial class Playground
 {
     public void Setup()
     {
-        Screen("JorCademy Playground", 800, 600);
+        Screen("Playground", 800, 600);
     }
 
     public void Draw()
     {
-        Backdrop(Color.White);
-        Ellipse(Color.Red, 100, 100, 200, 100);
-        Rectangle(Color.Green, 200, 200, 400, 300);
-        Ellipse(Color.Gold, 400, 400, 300, 300);
+    
     }
 }
